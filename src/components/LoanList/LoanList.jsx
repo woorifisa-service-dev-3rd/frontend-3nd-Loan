ad7f820a6ea7b73a181e0f5d987661f3406f78f3
kimh7537 <kimh7537@gmail.com>
2024-08-01 16:46:24 +0900

    정렬

--- src/components/LoanList/LoanList.jsx
+++ src/components/LoanList/LoanList.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import React, { useState, useContext, useEffect } from 'react';
 import { LoanContext } from "../../contexts/Loancontext";
 import { LoanItem } from "./LoanItem";
 import FilterHeader from "@/components/LoanList/FilterHeader";
@@ -6,17 +6,25 @@ import DefaultLayout from "../../layouts/DefaultLayout";
 
 export const LoanList = () => {
   const loans = useContext(LoanContext);
-  console.log(`log : ${loans}`);
+  const [filteredLoans, setFilteredLoans] = useState(loans);
+
+  useEffect(() => {
+    setFilteredLoans(loans);
+  }, [loans]);
+
+  const handleFilterChange = (filtered) => {
+    setFilteredLoans(filtered);
+  };
 
   return (
     <div className="min-h-screen flex justify-center items-center p-1">
       <div className="w-screen h-screen flex flex-col">
         <div className="flex-grow overflow-hidden">
           <DefaultLayout>
-            <FilterHeader/>
+            <FilterHeader onFilter={handleFilterChange} />
           </DefaultLayout>
           <ul className="px-0 my-0 overflow-y-auto h-full scrollbar-hidden">
-            {loans.map(loan =>
+            {filteredLoans.map(loan =>
               <LoanItem key={loan.id} loan={loan} />
             )}
           </ul>
