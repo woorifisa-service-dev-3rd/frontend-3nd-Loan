56e45890466a0bfac9cd43f654fa335dbf2a12db
LMR <dlaudfuf33@gmail.com>
2024-08-01 11:39:15 +0900

    feat 대출상품 리스트 독립 스크롤링, 루트 배경변경, 아이템 애니메이션 추가

--- src/components/LoanList/LoanList.jsx
+++ src/components/LoanList/LoanList.jsx
@@ -3,16 +3,20 @@ import { LoanContext } from "../../contexts/Loancontext";
 import { LoanItem } from "./LoanItem";
 
 export const LoanList = () => {
-
   const loans = useContext(LoanContext);
   console.log(`log : ${loans}`);
 
   return (
-    <>
-        <ul className='px-0 my-8'>
-          {loans.map(loan =>
-            <LoanItem key={loans.id} loan={loan} />)}
-        </ul>
-    </>
+    <div className="min-h-screen flex justify-center items-center p-1">
+      <div className="w-screen h-screen flex flex-col">
+        <div className="flex-grow overflow-hidden">
+          <ul className="px-0 my-0 overflow-y-auto h-full scrollbar-hidden">
+            {loans.map(loan =>
+              <LoanItem key={loan.id} loan={loan} />
+            )}
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 }
