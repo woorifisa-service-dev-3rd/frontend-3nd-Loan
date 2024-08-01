2a45c8da085bc62892366585796ef358dca24824
안찬웅 <1stevering@naver.com>
2024-08-01 09:22:12 +0900

    feat:리액트 라우터, 페이지간 이동

--- src/getLoan/jobType.jsx
+++ src/getLoan/jobType.jsx
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
-import HeaderNav from './components/ui/HeaderNav';
 
-// const JobType = () => {
+const JobType = () => {
     
-//     return (
-//         <HeaderNav />
-//         <section className="job-section">
-          
-//         </section>
-//       );
-//     };
+  return (
+      <>
+        <HeaderNav />
+        <section className="job-section">
+          <h1 className="form-heading">
+            <strong>직업을 선택하세요</strong>
+          </h1>
+
+              하이!!
+
+        </section>
+      </>
+    );
+  };
     
-// export default JobType;
+export default JobType;
